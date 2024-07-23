d4aafe483c1adbd916af59a259f46907d669838e
Simon Laden <simon.laden@payara.fish>
2024-07-23 09:00:51 +0100

    FISH-7714 - add last changes

--- appserver/tests/payara-samples/samples/cleanboot/src/test/java/fish/payara/functional/cleanboot/MockAPIIT.java
+++ appserver/tests/payara-samples/samples/cleanboot/src/test/java/fish/payara/functional/cleanboot/MockAPIIT.java
@@ -4,23 +4,23 @@ import com.microsoft.playwright.*;
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
+import java.io.IOException;
 
 import fish.payara.samples.PayaraArquillianTestRunner;
 import fish.payara.samples.PayaraTestShrinkWrap;
 import fish.payara.samples.NotMicroCompatible;
+import java.nio.file.Paths;
 
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import java.sql.Timestamp;
 
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(PayaraArquillianTestRunner.class)
 @NotMicroCompatible
-public class CleanBootIT {
+public class MockApiIT {
 
     static private Page page;
 
@@ -31,11 +31,13 @@ public class CleanBootIT {
     }
 
     @BeforeClass
-    static public void openPage() {
+    static public void openPage() throws IOException {
         //Load the Admin Console
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch();
+        
         page = browser.newPage();
+        mockLogo();
         page.navigate("http://localhost:4848/");
         page.waitForSelector("table[role='presentation']", new Page.WaitForSelectorOptions().setTimeout(120000));
     }
@@ -44,62 +46,20 @@ public class CleanBootIT {
     static public void closePage() {
         page.close();
     }
+    
+    static public void mockLogo(){
+        // Intercept the route to the Payara logo
+        page.route("*/**/images/masthead-product_name_open-new.png", route -> {
+            route.fulfill(new Route.FulfillOptions()
+                    .setPath(Paths.get("src/test/resources/wildfly_icons_one-color-logo.png")));
+        });
+    }
 
     @Test
     @RunAsClient
     public void openAdminConsole() {
         AdminPage.gotoHomepage(page);
         assertThat(page).hasTitle("Payara Server Console - Common Tasks");
-    }
-
-    @Test
-    @RunAsClient
-    public void createInstanceTest() {
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        String logs = timestamp.toString() + " \n";
-
-        AdminInstance.createInstance(page, "testInstance1");
-        AdminInstance.startInstance(page, "testInstance1");
-
-        String[] logLevels = {"WARNING", "SEVERE", "ALERT", "EMERGENCY"};
-        logs = logs.concat(AdminInstance.collectLogs(page, "testInstance1", logLevels));
-
-        AdminInstance.stopInstances(page);
-        AdminInstance.deleteInstances(page);
-
-        timestamp = new Timestamp(System.currentTimeMillis());
-        logs = logs.concat(timestamp.toString()) + " \n";
-        System.out.println(logs);
-        boolean containsError = logs.contains("SEVERE") || logs.contains("ALERT") || logs.contains("EMERGENCY");
-        if (containsError) {
-            Assert.fail("Logs contain SEVERE or ALERT or EMERGENCY");
-        }
-    }
-
-    @Test
-    @RunAsClient
-    public void createDeploymentGroupTest() {
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        String logs = timestamp.toString() + " \n";
-        String[] instances = {"groupInstance1", "groupInstance2"};
-
-        AdminGroup.createGroup(page, "group1", instances);
-        AdminGroup.startGroups(page);
-
-        String[] logLevels = {"WARNING", "SEVERE", "ALERT", "EMERGENCY"};
-        logs = logs.concat(AdminInstance.collectLogs(page, "groupInstance1", logLevels));
-        logs = logs.concat(AdminInstance.collectLogs(page, "groupInstance2", logLevels));
-
-        AdminGroup.stopGroups(page);
-        AdminGroup.deleteGroups(page);
-        AdminInstance.deleteInstances(page);
-
-        timestamp = new Timestamp(System.currentTimeMillis());
-        logs = logs.concat(timestamp.toString()) + " \n";
-        System.out.println(logs);
-        boolean containsError = logs.contains("SEVERE") || logs.contains("ALERT") || logs.contains("EMERGENCY");
-        if (containsError) {
-            Assert.fail("Logs contain SEVERE or ALERT or EMERGENCY");
-        }
+        page.pause();
     }
 }
